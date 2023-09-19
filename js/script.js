8924c5c10c50cdcf2292ddcded6da567e05be540
LewisPass <lewis.pass@outlook.com>
2023-09-19 15:49:20 +0100

    Added an if statement to help fix errors on other pages

--- js/script.js
+++ js/script.js
@@ -1,33 +1,37 @@
 const email = document.getElementById("email");
 const button = document.querySelector(".message-btn");
 
-button.addEventListener("click", () => {
-  const userEmail = email.value.trim();
-  console.log(userEmail);
+if (button) {
+  button.addEventListener("click", () => {
+    const userEmail = email.value.trim();
+    console.log(userEmail);
 
-  try {
-    email.classList.add("invalid-input");
-    function validateEmail() {
-      // Checks the user has entered an email in a simple format of anystring@anystring.anystring whilst also checking for multiple '@' symbols.
-      var enteredEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    try {
+      email.classList.add("invalid-input");
+      function validateEmail() {
+        // Checks the user has entered an email in a simple format of anystring@anystring.anystring whilst also checking for multiple '@' symbols.
+        var enteredEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-      // .test searches for a string/pattern and returns true of false.
-      return enteredEmail.test(userEmail);
+        // .test searches for a string/pattern and returns true of false.
+        return enteredEmail.test(userEmail);
+      }
+
+      console.log(validateEmail(userEmail));
+
+      // Tests if the users email is valid in the function. If it does and is TRUE then 'Email address is valid' is logged
+      // however if the email is not valid when passed into the function then an alert pops up letting the user know
+      // their email is not valid. This is also logged to the console
+      if (validateEmail(userEmail)) {
+        console.log("Email address is valid.");
+      } else {
+        alert("Please enter a valid email address.");
+        console.log("Email address is not valid.");
+      }
+    } catch (error) {
+      console.log("Error during email validation:", error);
+      alert(
+        "An error occured during email validation. Please try again later."
+      );
     }
-
-    console.log(validateEmail(userEmail));
-
-    // Tests if the users email is valid in the function. If it does and is TRUE then 'Email address is valid' is logged
-    // however if the email is not valid when passed into the function then an alert pops up letting the user know
-    // their email is not valid. This is also logged to the console
-    if (validateEmail(userEmail)) {
-      console.log("Email address is valid.");
-    } else {
-      alert("Please enter a valid email address.");
-      console.log("Email address is not valid.");
-    }
-  } catch (error) {
-    console.log("Error during email validation:", error);
-    alert("An error occured during email validation. Please try again later.");
-  }
-});
+  });
+}
